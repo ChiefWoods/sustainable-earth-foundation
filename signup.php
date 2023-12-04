b21dd406c3d2b7d0813ada75a894910aeef7a3bb
xhtime <149760546+xhtime@users.noreply.github.com>
2023-12-04 18:56:12 +0800

    Sign up and forgot password page
    Login,Sign up and forgot password page are using the same login.css

--- signup.php
+++ signup.php
@@ -3,31 +3,39 @@
 <head>
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Sign Up</title>
     <link rel="stylesheet" href="./css/login.css">
     <link rel="stylesheet" href="./css/header.css">
     <link rel="stylesheet" href="./css/footer.css">
     <script src="./js/dropdown.js"></script>
+<style>
+    .container{
+        background-image: url("pic/agriculture.jpg");
+        
+    }
+    
+    </style>
 </head>
 <body>
 
 <?php include 'header.php'; ?>
 
-<div class="container">
+<div class="container" >
     <div class="left-container">
-        <img src="icon/user.png">
         <p>
-            <h2>Welcome back!</h2>
+            <h2>Join us today!</h2>
         </p>
+        <p>Sign up now to become a member</p>
         <form>
             <input type="text" id="username" name="username" placeholder="Username">
+            <input type="text" id="phoneNum" name="phoneNum" placeholder="Phone Number">
+            <input type="text" id="Email" name="Email" placeholder="Email">
             <input type="text" id="password" name="password" placeholder="Password">
+            <input type="text" id="comPassword" name="comPassword" placeholder="Comfirm Password">
         </form>
-        <button class="login-btn" id="login" value="Login">Login</button>
-        <p>Forgot password? <a href="forgotpwd.php"><b>Click here</b></a></p>
-        <div class="signup">
-            <p>Don't have an account? <a href="signup.php"><b>Sign Up</b></a></p>
-        </div>
+        <button class="signup-btn" id="login" value="Sign Up">Sign Up</button>
+        <p>Already have an account? <a href="login.php"><b>Login</b></a></p>
+ 
     </div>
 </div>
 
