ce17fbf927a7e9ccb6dd991d9b17f5071f1bdeb4
xhtime <149760546+xhtime@users.noreply.github.com>
2023-12-10 22:57:56 +0800

    Update mytb.sql

--- sql/mytb.sql
+++ sql/mytb.sql
@@ -3,7 +3,7 @@ USE mytb;
 
 -- Drop tables if they exist
 DROP TABLE IF EXISTS redemption;
-DROP TABLE IF EXISTS voucher;
+DROP TABLE IF EXISTS reward;
 DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS tb_user;
 DROP TABLE IF EXISTS tb_user_role;
@@ -21,8 +21,8 @@ INSERT INTO tb_user_role VALUES (2, 'user');
 CREATE TABLE tb_user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_role_id INT DEFAULT 2,
-    username VARCHAR(20) UNIQUE, -- Ensure a UNIQUE index on username
-    phone_num BIGINT(20),
+    username VARCHAR(20) UNIQUE,
+    phone_num VARCHAR(20),
     email VARCHAR(50),
     password VARCHAR(50),
     confirm_password VARCHAR(50),
@@ -30,10 +30,9 @@ CREATE TABLE tb_user (
     FOREIGN KEY (user_role_id) REFERENCES tb_user_role(id)
 );
 
-
-INSERT INTO tb_user VALUES (NULL, 1, 'admin1', 0123456789, 'admin1@gmail.com', '123456', '123456', 0);
-INSERT INTO tb_user VALUES (NULL, 2, 'user1', 0123456789, 'user1@gmail.com', '123456', '123456', 5000);
-INSERT INTO tb_user VALUES (NULL, 2, 'user2', 0123456789, 'user2@gmail.com', '123456', '123456', 0);
+INSERT INTO tb_user VALUES (NULL, 1, 'admin1', '0123456789', 'admin1@gmail.com', '123456', '123456', 0);
+INSERT INTO tb_user VALUES (NULL, 2, 'user1', '0123456789', 'user1@gmail.com', '123456', '123456', 5000);
+INSERT INTO tb_user VALUES (NULL, 2, 'user2', '0123456789', 'user2@gmail.com', '123456', '123456', 0);
 
 -- Create post table
 CREATE TABLE post (
@@ -41,41 +40,32 @@ CREATE TABLE post (
     username VARCHAR(20),
     date DATETIME DEFAULT CURRENT_TIMESTAMP,
     title VARCHAR(50),
-    content VARCHAR(150), -- Corrected data type here
+    content TEXT,
     upvote INT DEFAULT 0,
     downvote INT DEFAULT 0,
     FOREIGN KEY (username) REFERENCES tb_user(username)
 );
 
-
--- Create voucher table
-CREATE TABLE voucher (
+-- Create reward table
+CREATE TABLE reward (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    voucher_code VARCHAR(20),
+    reward_code VARCHAR(20),
     points INT,
-    INDEX idx_voucher (voucher_code, points) -- Add an index on the referenced columns
+    INDEX idx_reward (reward_code, points)
 );
 
-
-INSERT INTO voucher VALUES (1, '$5.00OFF', 500);
-INSERT INTO voucher VALUES (2, '$10.00OFF', 1000);
-INSERT INTO voucher VALUES (3, '$15.00OFF', 1500);
-INSERT INTO voucher VALUES (4, '$20.00OFF', 2000);
+INSERT INTO reward VALUES (1, '$5.00OFF', 500);
+INSERT INTO reward VALUES (2, '$10.00OFF', 1000);
+INSERT INTO reward VALUES (3, '$15.00OFF', 1500);
+INSERT INTO reward VALUES (4, '$20.00OFF', 2000);
 
 -- Create redemption table
 CREATE TABLE redemption (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(20),
     used_points INT,
-    voucher_code VARCHAR(20),
+    reward_id INT,
     redemption_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (username) REFERENCES tb_user(username),
-    FOREIGN KEY (voucher_code, used_points) REFERENCES voucher(voucher_code, points)
+    FOREIGN KEY (reward_id) REFERENCES reward(id)
 );
-
-
--- Add foreign key constraint between redemption and voucher
-ALTER TABLE redemption
-ADD CONSTRAINT fk_voucher
-FOREIGN KEY (voucher_code, used_points)
-REFERENCES voucher(voucher_code, points);
