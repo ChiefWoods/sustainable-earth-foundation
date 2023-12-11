cfd7c4b5bb83bf953092b37f9a7b8b318b94d7e6
Chii Yuen <chii.yuen@hotmail.com>
2023-12-11 19:14:51 +0800

    Reformat footer

---
+++ src/php/components/footer.php
@@ -0,0 +1,58 @@
+<footer>
+  <div>
+    <div class="head">
+      <div id="links-socials">
+        <img src="../../assets/images/logo.png" alt="Logo" class="logo">
+        <div id="quick-links">
+          <div>
+            <a href="../views/index.php#hero">Goals</a>
+            <a href="../views/index.php#targets-indicators">Targets & Indicators</a>
+            <a href="../views/index.php#timeline">Timeline</a>
+          </div>
+          <div>
+            <a href="../views/index.php#purposes">Purposes</a>
+            <a href="../views/index.php#upcoming">Events</a>
+          </div>
+        </div>
+        <div id="social-links">
+          <span>Follow Us</span>
+          <a href="#">
+            <img src="../../assets/icons/facebook/facebook_blue.svg" alt="Facebook" class="social-icon">
+          </a>
+          <a href="#">
+            <img src="../../assets/icons/instagram/instagram_white.svg" alt="Instagram" class="social-icon">
+          </a>
+        </div>
+      </div>
+      <div id="newsletter">
+        <span>Let's Stay Connected!</span>
+        <form>
+          <input type="text" id="email-input" placeholder="Your Email">
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+    <div class="bottom">
+      <div id="more-links">
+        <a href="#">Contact Us</a>
+        <div class="divider"></div>
+        <a href="#">Fraud Alert</a>
+        <div class="divider"></div>
+        <a href="#">Privacy Notice</a>
+        <div class="divider"></div>
+        <a href="#">Terms of Use</a>
+      </div>
+      <span>&copy; Sustainable Earth Foundation. All Rights Reserved. </span>
+    </div>
+  </div>
+  <div>
+    <span class="made-by">Copyright @
+      <script>
+        document.write(new Date().getFullYear())
+      </script> ChiefWoods & xhtime
+    </span>
+    <a class="github-link" href="https://github.com/ChiefWoods/sustainable-earth-foundation" target="_blank">
+      <img src="../../assets/icons/github/github.svg" class="github-icon" alt="GitHub">
+    </a>
+  </div>
+</footer>
