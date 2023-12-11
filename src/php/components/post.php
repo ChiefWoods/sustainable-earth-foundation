31a94032c0cf5ae87d238d7db3709563989df1af
Chii Yuen <chii.yuen@hotmail.com>
2023-12-12 02:05:10 +0800

    Add sticky wall page

---
+++ src/php/components/post.php
@@ -0,0 +1,198 @@
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nisi earum soluta vel, illo corrupti magnam debitis pariatur.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor sit amet nisi earum soluta vel, illo corrupti magnam debitis pariatur.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem adipisicing elit. Facilis nisi earum soluta vel, illo corrupti magnam debitis pariatur.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor sit amet.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor sit amet pariatur.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis corrupti magnam debitis pariatur.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor sit amet.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor nisi earum soluta vel, illo corrupti magnam debitis pariatur.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor sit amet consectetur illo corrupti magnam debitis pariatur.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
+<div class="post">
+  <div class="top">
+    <span class="username">user1</span>
+    <span class="date">12/10/2023</span>
+  </div>
+  <div class="middle">
+    <h3 class="post-title">Post title</h3>
+    <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, explicabo qui. Magnam debitis in, quo quibusdam illum maxime ad et! Quasi explicabo aut dolorum.</p>
+  </div>
+  <div class="votes">
+    <button id="upvote-btn">
+      <img src="../../assets/icons/upvote/upvote.svg" alt="Upvote" class="vote-icon">
+    </button>
+    <button id="downvote-btn">
+      <img src="../../assets/icons/downvote/downvote.svg" alt="Downvote" class="vote-icon">
+    </button>
+  </div>
+</div>
---
+++ src/php/views/sticky_wall.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sticky Wall | Sustainable Earth Foundation</title>
+  <link rel="stylesheet" href="../../css/header_footer.css">
+  <link rel="stylesheet" href="../../css/sticky_wall.css">
+  <link rel="shortcut icon" href="../../assets/icons/favicon.png" type="image/x-icon">
+</head>
+
+<body>
+  <?php include '../components/header.php'; ?>
+  <main>
+    <img src="../../assets/backgrounds/sticky_wall_bg.jpg" alt="Groups of people smiling">
+    <section id="sticky-wall">
+      <div id="head">
+        <div id="title">
+          <img src="../../assets/icons/bulletin_board/bulletin_board.svg" alt="Sticky Wall">
+          <h2>Sticky Wall</h2>
+        </div>
+        <button id="create-post-btn" class="btn">
+          <img src="../../assets/icons/add_post/add_post_white.svg" alt="Create Post">
+          <span>Create Post</span>
+        </button>
+      </div>
+      <div id="wall">
+        <?php include '../components/post.php'; ?>
+      </div>
+    </section>
+  </main>
+  <?php include '../components/footer.php'; ?>
+</body>
+
+</html>
